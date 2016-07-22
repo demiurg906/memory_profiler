b42c6b2b3c38d696d4f3b69286e519ee04ea1911
Dmitriy Novozhilov <dimon2x@gmail.com>
2016-07-22 09:35:05 +0300

    ADD timer to example

--- examples/fast_tracemalloc_example.py
+++ examples/fast_tracemalloc_example.py
@@ -1,3 +1,5 @@
+from time import time
+
 from tracemalloc_profiler import profile
 
 
@@ -7,5 +9,16 @@ def f():
     b = [2] * (10 ** 6)
     return a, b
 
+
+@profile()
+def g():
+    a = {}
+    for i in range(100000):
+        a[i] = i
+    return a
+
 if __name__ == '__main__':
+    t = time()
     f()
+    g()
+    print('time of work is {0:.2f} s'.format(time() - t))
