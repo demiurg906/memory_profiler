4a0d1f3031c1a818ed44d929750cf290d2a948b9
Dmitriy Novozhilov <dimon2x@gmail.com>
2016-07-22 13:51:32 +0300

    ADD ``TraceProfiler`` with tracer using ``tracemalloc``

--- tracemalloc_profiler.py
+++ tracemalloc_profiler.py
@@ -1,7 +1,6 @@
 import inspect
+import sys
 import tracemalloc
-
-from collections import namedtuple
 
 from memory_profiler import show_results
 
@@ -13,11 +12,14 @@ _PREFIXES = {'B': _TWO_10 ** 2, 'KiB': _TWO_10, 'MiB': 1, 'GiB': 1 / _TWO_10}
 def profile(func=None, precision=4, stream=None):
     if func is not None:
         def wrapper(*args, **kwargs):
-            tracemalloc.start()
+            prof = TraceProfiler()
+
+            prof.enable()
             val = func(*args, **kwargs)
-            prof = get_info(func, tracemalloc.take_snapshot(), precision)
+            prof.disable()
+
+            prof.init_info(func, precision)
             show_results(prof, stream=stream, precision=precision)
-            tracemalloc.stop()
             return val
 
         return wrapper
@@ -28,27 +30,50 @@ def profile(func=None, precision=4, stream=None):
         return inner_wrapper
 
 
-def get_info(func, snapshot, precision):
-    sourcefile = inspect.getsourcefile(func)
-    traces = snapshot.traces
-    source_lines, fst_line = inspect.getsourcelines(func)
-    lines = {lineno: 1 / 10 ** (precision + 1)
-             for lineno in range(fst_line, fst_line + len(source_lines))}
-    for trace in traces:
-        tr = str(trace)
-        if tr.startswith(sourcefile):
-            line, mem = _parse(tr)
-            lines[line] += mem
-    for lineno in range(fst_line + 1, fst_line + len(source_lines)):
-        lines[lineno] += lines[lineno - 1]
-    Prof = namedtuple('Prof', ['code_map'])
-    lines = list(sorted(lines.items(), key=lambda t: t[0]))
-    return Prof({sourcefile: lines})
+class TraceProfiler:
+    def __init__(self):
+        self.code_map = None
+        self._snapshot = None
+        self._original_trace_function = None
+        self._frame = []
 
+    def enable(self):
+        tracemalloc.start()
+        self._original_trace_function = sys.gettrace()
+        sys.settrace(self.trace)
 
-def _parse(trace):
-    *filename, lineno, size = trace.split(':')
-    lineno = int(lineno)
-    mem, prefix = size.split()
-    mem = float(mem) / _PREFIXES[prefix]
-    return lineno, mem
+    def disable(self):
+        sys.settrace(self._original_trace_function)
+        tracemalloc.stop()
+
+    def trace(self, frame, event, arg):
+        print(event)
+        self._frame.append(frame)
+        if event == 'return':
+            self._snapshot = tracemalloc.take_snapshot()
+        if self._original_trace_function is not None:
+            self._original_trace_function(frame, event, arg)
+        return self.trace
+
+    def init_info(self, func, precision):
+        sourcefile = inspect.getsourcefile(func)
+        traces = self._snapshot.traces
+        source_lines, fst_line = inspect.getsourcelines(func)
+        lines = {lineno: 1 / 10 ** (precision + 3)
+                 for lineno in range(fst_line, fst_line + len(source_lines))}
+        for trace in traces:
+            tr = str(trace)
+            if tr.startswith(sourcefile):
+                line, mem = self._parse(tr)
+                lines[line] += mem
+        for lineno in range(fst_line + 1, fst_line + len(source_lines)):
+            lines[lineno] += lines[lineno - 1]
+        lines = list(sorted(lines.items(), key=lambda t: t[0]))
+        self.code_map = {sourcefile: lines}
+
+    def _parse(self, trace):
+        *filename, lineno, size = trace.split(':')
+        lineno = int(lineno)
+        mem, prefix = size.split()
+        mem = float(mem) / _PREFIXES[prefix]
+        return lineno, mem
